33a0a869f46d0048b012967b9bf2cd1575edfa32
Dima Noskov <windn12@gmail.com>
2020-01-10 17:54:47 +0500

    protect structure files

--- base.py
+++ base.py
@@ -1,31 +1,16 @@
-import pyodbc
-from datetime import datetime
-from os.path import split
-from os import getcwd
+import xlrd
 
 
-conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=D:\python\Svod\Report.accdb;')
-cursor = conn.cursor()
-# s1 = 43598.0 print(split(opl_file)[1].endswith('xlsx'))
-# s2 = 43591.0
-# s3 = 'куцй'
-s = [515, 520, 513, 525]
-# print(s3)
-# # print(f'''
-# #                      INSERT INTO table3 (First_Name, Last_Name, Age)
-# #                      VALUES("{s1}", '{s2}', {s3})
-# # 513, 43591.0, куцй, 605681.55
-# #                   ''')
-# cursor.execute(f'''INSERT INTO Pos (Nom_pact, Date_pact, Supplier, Sum)
-#                    VALUES (513, {s2}, '{s3}',  605681.55)
-#                   ''')
-#
-#
-#
-# conn.commit()
-for i in s:
-    row = cursor.execute(f'select 1 from pos where Nom_pact={i}')
-    print(cursor.fetchall())
-    print(list(row))
-#if (515) in cursor.fetchall():
-#print('yes')
+book = xlrd.open_workbook('Pos.xlsx')
+sheet = book.sheet_by_index(0)
+row = sheet.row_values(0)
+opl_row = ['№\nп/п', 'Поставщик', 'Договор №', 'Договор дата', 'Документ', '', '']
+pos_row = ['Наименование товаров, работ, услуг',
+           'Инициатор',
+           'Номер договора',
+           'Дата',
+           'Поставщик',
+           'Статус договора',
+           'Сумма договора']
+if row == pos_row:
+    print('yes')
